f5854154931e088a85d6f91d8a4755edee9664b3
taslangraham <tgraham@realdecoy.com>
2022-03-12 11:26:09 -0500

    align with template release 1.0.2-alpha

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -3,4 +3,4 @@ export const SUCCESS_EXIT_CODE = 0;
 export const OPTION_RECURSIVE = '-r';
 export const UTF8 = 'utf8';
 // Place release tag here
-export const releaseTag = '';
+export const releaseTag = '1.0.2-alpha';
